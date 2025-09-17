1c1341644f2547b0f4286e01adc3340356f99099
ef2739 <ef2739@nyu.edu>
2025-09-17 17:13:01 +0200

    Update app.js

--- HOMEWORK WEEK 2- 2 LINES STORY WEBPAGE/app.js
+++ HOMEWORK WEEK 2- 2 LINES STORY WEBPAGE/app.js
@@ -1,14 +1,9 @@
 //steps
-//1 listen to the event of clicking the button
 
+//1 listen to the event of clicking the button
 let button;
 button = document.getElementById('button');
-console.log(button);
-
-
-//2 listen to event click on te button
 button.addEventListener("click", bgcolour);
-
 
 //2 make the background color change to black 
 function bgcolour(){
@@ -16,6 +11,8 @@ function bgcolour(){
     document.body.style.background = "#000000"
 }
 
+//3 make the second sentence appear 
+//???
 
-//3 make the second sentence appear
 //4 change the color of the second sentence
+//??
