d0fb579da02297c135bd295369ebba7f11e1abbc
ef2739 <ef2739@nyu.edu>
2025-09-17 17:43:19 +0200

    Update app.js

--- HOMEWORK WEEK 2- 2 LINES STORY WEBPAGE/app.js
+++ HOMEWORK WEEK 2- 2 LINES STORY WEBPAGE/app.js
@@ -1,6 +1,8 @@
 //steps
 
-alert("Content may be triggering // abuse, phsichological violence");
+// alert: TW
+alert("Content may be triggering // abuse, psychological violence");
+
 
 //1 listen to the event of clicking the button
 
@@ -8,17 +10,19 @@ let button;
 button = document.getElementById('button');
 console.log(button);
 
-//2 listen to event click on the button
+
+//2 listen to event click on te button
 button.addEventListener("click", bgcolour, fscolour);
 
+
 //2 make the background color change to black 
-function bgcolour(){
-   // console.log("increasing counter");
+function bgcolour() {
+    // console.log("increasing counter");
     document.body.style.background = "#000000";
 }
 
-//3 change the color of the second sentence to yellow
-function fscolour (){
+
+//3 change the color of the second sentence
+function fscolour() {
     document.getElementById("second-sentence").style.color = "#FFFF00";
 }
-
