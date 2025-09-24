9b774712c81793111cfbc1fbd69ef434ecdefdda
ef2739 <ef2739@nyu.edu>
2025-09-25 01:41:35 +0200

    Add files via upload

--- PROJECT1-connections_lab/app.js
+++ PROJECT1-connections_lab/app.js
@@ -1,28 +1,43 @@
-console.log("1 - Loading!");
+//create a pop up with trigger warning
+let a = "trigger warning: death, violence";
+alert(a);
+
 
 //Wait for the entire page to load before making the fetch requests
-window.addEventListener('load', () => {
-
+window.addEventListener("load", () => {
     console.log("The page is ready!");
-
-
-
-
     //Fetch images data
     fetch('images.json')
         .then(response => response.json())
         .then(data => {
+            // console.log(data.wars[7].image_url);//
+            //console.log(data.wars)
+
+            //    let images = data.wars;
+            //    console.log(images);
+
+            let warsArray = data.image_url
+            let randomNumber = Math.floor(Math.random() * warsArray.length);
+            let nameElement = document.getElementById("selected_picture");
+            nameElement.innerHTML = data.wars[randomNumber].image_url;
+        })
+
+
+    let button = document.getElementsByClassName("year-selector");
+    button.addEventListener("click", function () {
+        console.log("button is clicked!");
+
+        fetch("images.json")
+        .then(response=> response.json())
+        .then(data=>{
             console.log(data);
 
-            let images = data.images;
-            console.log(images);
+            let imageElement = document.getElementById("selected_picture");
+            imageElement.innerHTML= data.wars.image_url;
+            // imageElement.src = data.wars.image_url;
 
-            let randomNum = Math.floor(Math.random() * images.length);
-            let randomimages = images[randomNum].name;
-            console.log(randomimages);
+        })
 
-            //add the picture to the page
-            //??????  let nameEl = document.querySelector('#data-container-1');
-            //?????nameEl.innerText = randomimages;
-        });
-});
+
+    })
+})
