76e2b1773dee94279c856cebd640b844c320374c
ef2739 <ef2739@nyu.edu>
2025-09-29 17:54:56 +0200

    Add files via upload

--- PROJECT1-connections_lab/app.js
+++ PROJECT1-connections_lab/app.js
@@ -1,43 +1,90 @@
 //create a pop up with trigger warning
-let a = "trigger warning: death, violence";
-alert(a);
+// let a = "trigger warning: death, violence";
+// alert(a);
 
+
+let warData;
 
 //Wait for the entire page to load before making the fetch requests
 window.addEventListener("load", () => {
     console.log("The page is ready!");
-    //Fetch images data
+    //Fetch the war data data
     fetch('images.json')
         .then(response => response.json())
         .then(data => {
             // console.log(data.wars[7].image_url);//
-            //console.log(data.wars)
+            console.log(data)
 
-            //    let images = data.wars;
-            //    console.log(images);
+            warData = data;
 
-            let warsArray = data.image_url
-            let randomNumber = Math.floor(Math.random() * warsArray.length);
-            let nameElement = document.getElementById("selected_picture");
-            nameElement.innerHTML = data.wars[randomNumber].image_url;
+            // add info + image to the page
+
+
+            updatePageInfo(warData.wars);
         })
 
 
-    let button = document.getElementsByClassName("year-selector");
+    //event listener on the button
+
+    let button = document.querySelector(".year-selector");
     button.addEventListener("click", function () {
         console.log("button is clicked!");
-
-        fetch("images.json")
-        .then(response=> response.json())
-        .then(data=>{
-            console.log(data);
-
-            let imageElement = document.getElementById("selected_picture");
-            imageElement.innerHTML= data.wars.image_url;
-            // imageElement.src = data.wars.image_url;
-
-        })
-
+     updatePageInfo(warData.wars);
 
     })
+
+
 })
+
+
+
+
+//function to add info to the page
+function updatePageInfo(warList) {
+    console.log("Working!");
+    console.log(warList);
+
+    //Pick a random object in the array
+    let randomNumber = Math.floor(Math.random() * warList.length);
+    console.log(randomNumber);
+    let randomWar = warList[randomNumber];
+    console.log(randomWar);
+
+    //IMAGE CODE
+
+    //add image to the page
+    let imageElement = document.createElement('img');
+
+    imageElement.src = randomWar.image_url;
+    imageElement.classList.add("imagedata");
+
+    // let testURL = "https://encyclopedia.ushmm.org/images/large/50083758-e1cc-458c-86a0-173b2f517d99.jpg";
+    // imageElement.src = testURL;
+
+    //select for image container
+    let imageContainer = document.getElementById("image-container");
+    //clear out any current image
+    imageContainer.innerHTML = "";
+    //append new image to the page
+    imageContainer.appendChild(imageElement);
+
+    //TEXT CODE
+
+    //create element for conflict name
+    let conflictElement = document.createElement('h2');
+    conflictElement.classList.add('conflict-text');
+    conflictElement.innerText = randomWar.conflict;
+
+    //create element for conflict description
+    let descriptionElement = document.createElement('p');
+    descriptionElement.innerText = randomWar.description;
+
+    //add name + description to the page
+    let descriptionContainer = document.getElementById('description-container');
+    //clear out any current info
+    descriptionContainer.innerHTML = "";
+    //append both elements inside the container
+    descriptionContainer.appendChild(conflictElement);
+    descriptionContainer.appendChild(descriptionElement);
+
+}
